65ce1af8b3db18e6bc0813f5f55b4c33e79db336
Naumov <naumov.a.n@mail.ru>
2024-09-22 16:09:23 +0700

    create file CMakeLists.txt

---
+++ RobotMotorsControl/include/Motor.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class Motor
+{
+private:
+    
+public:
+    Motor();
+    ~Motor();
+
+    void moveForward(int speed);
+    void moveBackward(int speed);
+    void turnLeft(int speed);
+    void turnRight(int speed);
+    void stop();
+};
+
+
